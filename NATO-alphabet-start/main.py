8af48cfceb23ebc5ded960bf2ee411158bc05396
unknown <priscillawambui8@gmail.com>
2024-03-10 14:20:56 +0300

    Added a try and except functionality to deter users from getting errors when they input numbers or symbols

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -3,6 +3,17 @@ nato=pandas.read_csv("nato_phonetic_alphabet.csv")
 # print(nato)
 nato_values={row.letter:row.code for (index,row) in nato.iterrows()}
 # print(nato_values)
-user_word= input("Please enter a word: ").upper()
-output_list= [nato_values[letter]for letter in user_word]
-print(output_list)
+
+def generate_phonetic():
+    user_word= input("Please enter a word: ").upper()
+    try:
+        output_list=[nato_values[letter] for letter in user_word]
+
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
